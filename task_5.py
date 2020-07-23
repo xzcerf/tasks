c770f1bcbf5a60069744f08c5c881cbefc0e46a5
xzcerf <68645869+xzcerf@users.noreply.github.com>
2020-07-23 22:37:10 +0300

    Add files via upload

---
+++ task_5.py
@@ -0,0 +1,3 @@
+n = int(input())
+k = int(input())
+print(f'Яблок у студентов: {k // n}, в корзине: {k % n}')
---
+++ task_7.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+n1 = a*3600 + b*60 + c
+n2 = d*3600 + e*60 + f
+print(n2 - n1)
